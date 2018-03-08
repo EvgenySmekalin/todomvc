789923bd2e14cd31a470bc0e3875aad29297de32
EvgenySmekalin <bakaExpire@mail.ru>
2018-03-08 16:30:50 +0700

    Init commit

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+jQuery(function ($) {
+    'use strict';
+
+    ('#new-todo').keyup(function(event){
+        var keyCode = (event.keyCode ? event.keyCode : event.which);
+        if (keyCode === 13) {
+
+        }
+    });
+
+});
